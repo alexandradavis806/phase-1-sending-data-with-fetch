146947607647dd94409226212d528f3f02eecc46
Alexandra Davis <alex.davis806@gmail.com>
2022-03-27 15:29:02 -0700

    commit tree ba7976e907da8fc6ce1b475e055ed97c9e53be0e

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+
+
+const submitData = (name, email) => {
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            accept: 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(r => r.json())
+    .then(newObj => {
+        const list = document.createElement('ul')
+        const listItem = document.createElement('li')
+        listItem.innerHTML = newObj.id
+        document.body.append(list)
+        list.append(listItem)
+    })
+    .catch(error => {
+        const list = document.createElement('ul')
+        const listItem = document.createElement('li')
+        listItem.innerHTML = error.message
+        document.body.append(list)
+        list.append(listItem)
+    })
+
+
+    return fetch()
+}
+
+// submitData();
